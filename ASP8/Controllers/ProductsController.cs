b0a8615efd142ead4bd48a5b4f4f958aca52c11c
USER <USER@KOMPUTER>
2024-04-17 20:01:31 +0300

    Добавьте файлы проекта.

---
+++ ASP8/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using ASP8.Models;
+
+namespace ASP8.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+public class ProductsController : Controller
+{
+    public IActionResult Index()
+    {
+        var products = new List<Product>();
+        var random = new Random();
+
+        for (int i = 1; i <= 5; i++)
+        {
+            products.Add(new Product
+            {
+                ID = i,
+                Name = $"Product {i}",
+                Price = random.Next(50, 150),
+                CreatedDate = DateTime.Now.AddDays(-random.Next(0, 30))
+            });
+        }
+
+        return View(products);
+    }
+}
